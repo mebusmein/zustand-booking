a355f4435733984264a3a3b45cfc68bbd36a4e9b
Tim Mein <mebusmein@gmail.com>
2025-04-13 15:22:14 +0200

    refactor: remove unused page and store components for hotel booking

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { MultiStepBookingForm } from '@/components/MultiStepBookingForm';
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <MultiStepBookingForm />
-    </main>
-  );
-}
